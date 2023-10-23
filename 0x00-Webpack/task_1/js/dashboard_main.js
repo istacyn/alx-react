a89d9d1e21a9bf873b3c50d09a100f9ce73feaa5
Ivy <ivystacyn@gmail.com>
2023-10-23 20:11:02 +0300

    Task 1

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+const $body = $('body');
+$body.append('<p>Holberton Dashboard</p>');
+$body.append('<p>Dashboard data for the students</p>');
+$body.append('<button>Click here to get started</button>');
+$body.append('<p id="count"></p>');
+$body.append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+
+function updateCounter() {
+    count += 1;
+    $("#count").html(`${count} clicks on the button`);
+};
+
+$('button').on('click', _.debounce(updateCounter, 500));
