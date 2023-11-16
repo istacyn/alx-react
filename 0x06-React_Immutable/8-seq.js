0cbdffdd80f0e2ef0cb5f99371b9466800d12a93
Ivy <ivystacyn@gmail.com>
2023-11-16 11:21:46 +0000

    Task 8

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,14 +1,15 @@
 import { Seq } from 'immutable';
 
-export default function printBestStudents(object) {
-  const grades = Seq(object)
-    .filter(student => student.score > 70)
-    .map(student => ({
-      ...student,
-      firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
-      lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
-    }))
-    .toObject();
+export default function printBestStudents(grades) {
+	const bestStudents = Seq(grades)
+		.filter((student) => student.score >= 70)
+		.map((student) => ({
+			score: x.score,
+			firstName:
+				student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
+			lastName:
+				student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
+		}));
 
-  console.log(grades);
+	console.log(bestStudents.toObject());
 }
